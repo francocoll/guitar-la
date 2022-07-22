22564c6be6a5561a2483b237793c53ffda0ea5f9
franco <francoadrian.coll2012@gmail.com>
2022-07-22 18:09:42 -0300

    ddd

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import styles from '../styles/Header.module.css'
 
+
 const Header = () => {
     return (
         <header className={styles.header}>
